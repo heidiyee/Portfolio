7aaffa1afe7d96e3b542746e7ab2d09807bcfa25
heidiyee <heidi.n.yee@gmail.com>
2016-07-07 15:47:53 -0700

    Added a menu.js file that animates the side nav based on a media query. If the width is less than 500, the side nav will slide out the full width of the browser window. Otherwise, the side nav will slide out 250px.

--- ui/menu.js
+++ ui/menu.js
@@ -1,21 +1,32 @@
-function openNav() {
-    document.getElementById("nav-links").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
-}
-
 $(document).ready(function(){
+    var mediaQuery = window.matchMedia( "(max-width: 500px)" );
+
 	$('#nav-icon').click(function(){
         $(this).toggleClass('open');
 
         var width = document.getElementById("nav-links").style.width
         var mainRightMargin = document.getElementById("main").style.marginRight
+        console.log(width)
 
-        if (width == "250px" && mainRightMargin == "250px") {
-            document.getElementById("nav-links").style.width = "0px";
-            document.getElementById("main").style.marginRight = "0px";
+        if (mediaQuery.matches) {
+            if (width == "100%") {
+                document.getElementById("nav-links").style.width = "0px";
+                document.getElementById("nav-icon").style.position = "";
+            } else {
+                //document.getElementById("nav-links").style.top = "50px";
+                document.getElementById("nav-links").style.width = "100%";
+                document.getElementById("nav-icon").style.zIndex = "2";
+                document.getElementById("nav-icon").style.position = "fixed";
+                // document.getElementById("main").style.marginRight = "100%";
+            }
         } else {
-            document.getElementById("nav-links").style.width = "250px";
-            document.getElementById("main").style.marginRight = "250px";
+            if (width == "250px" && mainRightMargin == "250px") {
+                document.getElementById("nav-links").style.width = "0px";
+                document.getElementById("main").style.marginRight = "0px";
+            } else {
+                document.getElementById("nav-links").style.width = "250px";
+                document.getElementById("main").style.marginRight = "250px";
+            }
         }
 	});
 });
