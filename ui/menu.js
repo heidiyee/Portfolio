b1c372c86a3957cbd4229289db9e90a71579600a
heidiyee <heidi.n.yee@gmail.com>
2016-09-08 15:27:25 -0700

    Changing to sass, cleaning up git

--- ui/menu.js
+++ ui/menu.js
@@ -1,32 +1,27 @@
-$(document).ready(function(){
+var sideNav = function() {
     var mediaQuery = window.matchMedia( "(max-width: 500px)" );
 
-	$('#nav-icon').click(function(){
+	$('#nav-icon').click(function() {
         $(this).toggleClass('open');
 
         var width = document.getElementById("nav-links").style.width
         var mainRightMargin = document.getElementById("main").style.marginRight
-        console.log(width)
 
-        if (mediaQuery.matches) {
-            if (width == "100%") {
-                document.getElementById("nav-links").style.width = "0px";
-                document.getElementById("nav-icon").style.position = "";
-            } else {
-                //document.getElementById("nav-links").style.top = "50px";
+        if ($(this).hasClass('open')) {
+            if (mediaQuery.matches) {
                 document.getElementById("nav-links").style.width = "100%";
                 document.getElementById("nav-icon").style.zIndex = "2";
-                document.getElementById("nav-icon").style.position = "fixed";
-                // document.getElementById("main").style.marginRight = "100%";
-            }
-        } else {
-            if (width == "250px" && mainRightMargin == "250px") {
-                document.getElementById("nav-links").style.width = "0px";
-                document.getElementById("main").style.marginRight = "0px";
             } else {
                 document.getElementById("nav-links").style.width = "250px";
                 document.getElementById("main").style.marginRight = "250px";
+                document.getElementById("nav-icon").style.marginRight = "270px";
             }
+        } else {
+            document.getElementById("nav-links").style.width = "0px";
+            document.getElementById("main").style.marginRight = "0px";
+            document.getElementById("nav-icon").style.marginRight = "2em";
         }
 	});
-});
+};
+
+$(document).ready(sideNav);
