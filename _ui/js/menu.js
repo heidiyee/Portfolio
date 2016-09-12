b0986a3ae494355cca1e5a5042244e8c0d0297be
heidiyee <heidi.n.yee@gmail.com>
2016-09-12 13:03:48 -0700

    Restarted from scratch and added sass to the project.

---
+++ _ui/js/menu.js
@@ -0,0 +1,9 @@
+var topMenu = function() {
+	$("#nav-icon").click(function(e) {
+        $(this).toggleClass("open");
+        $("#nav-links").toggleClass("popup");
+		//e.preventDefault();
+	});
+};
+
+$(document).ready(topMenu);
