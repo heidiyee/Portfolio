8eaacb99f6425c0740004ce8d6053497e67f12b5
heidiyee <heidi.n.yee@gmail.com>
2016-09-24 14:59:06 -0700

    finish with contact form and php. Need to validate that it works once its on the server.

--- _ui/js/submitPHP.js
+++ _ui/js/submitPHP.js
@@ -1,14 +1,54 @@
+function validateEmail(elementValue){
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(elementValue);
+}
+
 var submit = function() {
+    var name = $("#name");
+    var nameVal = name.val();
+    var email = $("#email");
+    var emailVal = email.val();
+    var emailValid = false;
+    var message = $("#message");
+    var messageVal = message.val();
+
+    console.log("hello" + nameVal + emailVal + messageVal);
+
+    name.on('input', function() {
+        name.removeClass('red');
+        nameVal = name.val();
+
+    });
+
+    email.on('input', function() {
+        email.removeClass('red');
+        emailVal = email.val();
+        emailValid = validateEmail(emailVal);
+        console.log(emailValid);
+    });
+
+    message.on('input', function() {
+        message.removeClass('red');
+        messageVal = message.val();
+    });
 
     $("#submit").click(function() {
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var message = $("#message").val();
-        console.log("called");
+
         // var dataString = 'name' + name + 'email' + email;
-        if (name == '' || email == '' || message == '') {
-            // alert("Please Fill All Fields");
-            console.log("not all fields filled");
+        if (nameVal == '' || emailVal == '' || messageVal == '' || emailValid == false) {
+            if (nameVal == "") {
+                console.log(nameVal);
+                name.addClass('red');
+            }
+            if (emailVal == "" || emailValid == false) {
+                console.log(emailVal);
+                email.addClass('red');
+            }
+            if (messageVal == "" ) {
+                console.log(messageVal);
+                message.addClass('red');
+            }
+
         } else {
                 // AJAX code to submit form.
             $.ajax({
@@ -19,10 +59,13 @@ var submit = function() {
                     console.log(data);
                     if (data.toLowerCase().indexOf("thanks") >= 0) {
                         $("#result").html(data);
+                    } else {
+                        $("#result").html("There was a problem sending your message, please try again");
                     }
                 }
             });
         }
+        $("#result").html("Please fill out all three fields");
         return false;
     });
 };
